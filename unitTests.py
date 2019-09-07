17dd16c93dd2a0f9b429bf55bc92ab01f5d7f398
Thiago Gomes <thiagoluiz92@gmail.com>
2019-09-07 19:38:20 -0300

    stage changes

--- unitTests.py
+++ unitTests.py
@@ -1,2 +1,6 @@
 import funcoes
-assert funcoes.timesig_filtra("localizacaocompassobpm3.csv")==(int,int,int)
+assert funcoes.entrada_csv("localizacaocompassobpm3.csv")
+assert funcoes.com_bpm()
+assert funcoes.timesig_filtra("localizacaocompassobpm3.csv")==[int,int,int]
+
+
