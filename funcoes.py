e5ae830532098030f2a0f5ac2bc00bb39264d739
Cristiano Gomes <crisbgomes@outlook.com>
2019-09-07 01:01:09 -0300

    indent

--- funcoes.py
+++ funcoes.py
@@ -9,9 +9,9 @@ def listaIntervalo(listaNota):
 def SegLoc(ListaLocalizacao, ListaIntervalo, ListaDuracao)
     if len(ListaLocalizacao) == len(ListaIntervalo) == len(ListaDuracao):
         for posicao1 in range(len(ListaIntervalo)):
-                for posicao2 in range(posicao1, len(ListaIntervalo)):
-                        print(ListaLocalizacao[posicao1], (posicao2+1) - posicao1, ListaIntervalo[posicao1:posicao2+1], ListaDuracao[posicao1:posicao2+1])
-else:
+            for posicao2 in range(posicao1, len(ListaIntervalo)):
+                print(ListaLocalizacao[posicao1], (posicao2+1) - posicao1, ListaIntervalo[posicao1:posicao2+1], ListaDuracao[posicao1:posicao2+1])
+    else:
         print("listas de tamanhos diferentes")
         print("len(ListaLocalizacao) ==", len(ListaLocalizacao), "len(listaintervalo) ==", len(ListaIntervalo), "len(listaduracao) ==", len(ListaDuracao)) 
 
