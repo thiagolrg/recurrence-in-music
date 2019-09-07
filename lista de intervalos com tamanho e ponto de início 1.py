570af594e4c27eeea1581761b63e54ca953cc9f3
ThiagoLRG <54580798+ThiagoLRG@users.noreply.github.com>
2019-09-06 23:02:09 -0300

    Add files via upload

---
+++ lista de intervalos com tamanho e ponto de início 1.py
@@ -0,0 +1,15 @@
+﻿listalocalizacao = [[1, 1], [1, 2], [1, 3], [1, 4], [2, 1], [2, 2], [2, 3], [2, 4]]
+listanota = [13, 15, 18, 10, 8, 20, 14]
+listaintervalo = list()
+
+# Gera a listaintervalo a partir da listanota
+
+for posicao in range(len(listanota)):
+	if posicao <= len(listanota)-2:
+		listaintervalo.append(listanota[posicao+1] - listanota[posicao])
+
+# monta todos os segmentos a partir da lista intervalo, imprimi a posição corresponde na listalocalização e tamanho
+
+for posicao1 in range(len(listaintervalo)):
+	for posicao2 in range(posicao1, len(listaintervalo)):
+		print(listalocalizacao[posicao1], (posicao2+1) - posicao1, listaintervalo[posicao1:posicao2+1])
