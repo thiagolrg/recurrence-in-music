52cc9c4c0daebb5a37038fc2435d845a5dec9848
Thiago Gomes <thiagoluiz92@gmail.com>
2019-09-07 10:43:58 -0300

    mudança na funcao timesig_filtra

--- funcoes.py
+++ funcoes.py
@@ -1,23 +1,23 @@
-#loop que monta ListaIntervalo a partir de ListaNota
-def lista_intervalo(listaNota):
+#monta lista_intervalo a partir de lista_nota
+def lista_intervalo(lista_nota):
     lista_int = []
-    for posicao in range(len(listaNota)):
-	    if posicao <= len(listaNota)-2:
-		    lista_int.append(listaNota[posicao+1] - listaNota[posicao])
+    for posicao in range(len(lista_nota)):
+	    if posicao <= len(lista_nota)-2:
+		    lista_int.append(lista_nota[posicao+1] - lista_nota [posicao])
     return lista_int
 
-#segmenta e localiza ListaIntervalo e ListaDuracao
-def SegLoc(ListaLocalizacao, ListaIntervalo, ListaDuracao):
-    if len(ListaLocalizacao) == len(ListaIntervalo) == len(ListaDuracao):
-        for posicao1 in range(len(ListaIntervalo)):
-            for posicao2 in range(posicao1, len(ListaIntervalo)):
-                print(ListaLocalizacao[posicao1], (posicao2+1) - posicao1, ListaIntervalo[posicao1:posicao2+1], ListaDuracao[posicao1:posicao2+1])
+#segmenta e localiza lista_intervalo e lista_duracao
+def seg_loc(lista_localizacao, lista_intervalo, lista_duracao):
+    if len(lista_localizacao) == len(lista_intervalo) == len(lista_duracao):
+        for posicao1 in range(len(lista_intervalo)):
+            for posicao2 in range(posicao1, len(lista_intervalo)):
+                print(lista_localizacao[posicao1], (posicao2+1) - posicao1, lista_intervalo[posicao1:posicao2+1], lista_duracao[posicao1:posicao2+1])
     else:
         print("listas de tamanhos diferentes")
-        print("len(ListaLocalizacao) ==", len(ListaLocalizacao), "len(listaintervalo) ==", len(ListaIntervalo), "len(listaduracao) ==", len(ListaDuracao)) 
+        print("len(lista_localizacao) ==", len(lista_localizacao), "len(lista_intervalo) ==", len(lista_intervalo), "len(lista_duracao) ==", len(lista_duracao)) 
 
-
-def compasso_uc(num,den,ppq):
+#calcula unidade de compasso de um timesignature
+def timesig_uc(num,den,ppq):
     if den == 0:
         UC = ppq*4*num
     elif den == 1:
@@ -33,23 +33,26 @@ def compasso_uc(num,den,ppq):
     return UC
 
 
-
-def compasso_nt(num,vuc):
+#calcula o numero de tempos de um timesignature
+def timesig_nt(num,vuc):
     if num == 2 or 3 or 4:
-        NT = vuc/num
+        NT = num
     elif num == 6 or 9 or 12:
-        NT = vuc/num/2
+        NT = num/2
     else:
         raise ValueError('numerador nao encontrado')
     return NT
 
 import csv
-def filtra_timesig(camimnho_csv):
+def Timesig_filtra(camimnho_csv):
+    ppq = int()
     timesignature = list()
     with open(camimnho_csv) as arquivo:
         reader = csv.reader(arquivo)
         for row in reader:
+            if ' Header' in row:
+                ppq = row[-1:-1]
             if ' Time_signature' in row:
-                timesignature.append(row)
+                timesignature.append(row[3:4], ppq)
     return timesignature
 
