b2ad44a9a381759bbf0087d670d621a06dad71d0
Thiago Gomes <thiagoluiz92@gmail.com>
2019-09-09 14:33:25 -0300

    fiz loops no modsiniciais que montam a localização da complista e da entrada limpa, devo ter feito modificações leves no funcoes tbm

--- modsiniciais.py
+++ modsiniciais.py
@@ -3,16 +3,39 @@
 #ainda estou mexendo nesse
 
 import funcoes
-import csv
+import copy
+
 entrada = funcoes.entrada_csv("localizacaocompassobpm3.csv")
-
 entradalimpa = funcoes.limpeza(entrada)
+ppq = funcoes.tira_ppq(entradalimpa)
+complista = funcoes.comp_lista(entradalimpa)
+tempolista = funcoes.tempo_lista(entradalimpa)
 
-ppq = int(entrada[0][-1])
-combpm = funcoes.com_bpm(entrada)
-timesig = funcoes.timesig_filtra(entrada)
+#faz uma lista copia da complista acrescendando localizacoes em cada compasso
+if complista[0][1] == 0:
+   compcomloc = []
+   compcomloc.append([[1, 1.0], complista[0]])
+   for posicao in range(len(complista)):
+      if posicao+1 < len(complista):
+         uc = funcoes.comp_uc(complista[posicao], ppq)
+         nt = funcoes.comp_nt(complista[posicao])
+         loc = (complista[posicao+1][1] - complista[posicao][1])/uc + compcomloc[posicao][0][0]
+         locc = int(loc)
+         loct = ((loc%1)*nt)+1
+         junta = [locc, loct]
+         junta = [junta, complista[posicao+1]]
+         compcomloc.append(junta)
+else:
+   raise ValueError('compasso nao comeca no inicio')
 
-for linha in entrada:
-   compasso_referencia = funcoes.compasso_referencia(linha,timesig)
-   Unidade_compasso = funcoes.timesig_uc(int(compasso_referencia[1]),int(compasso_referencia[2]),ppq)
-   linha[1] - compasso_referencia[0]
+entradacomloc = []
+ for linha in entradalimpa:
+   comp = funcoes.comp_ref(linha, compcomloc)
+   uc = funcoes.comp_uc(comp[1], ppq)
+   nt = funcoes.comp_nt(comp[1])
+   loc = (linha[1] - comp[1][1])/uc + comp[0][0]
+   locc = int(loc)
+   loct = ((loc%1)*nt)+1
+   junta = [locc, loct]
+   junta = [junta, linha]
+   entradacomloc.append(junta)
