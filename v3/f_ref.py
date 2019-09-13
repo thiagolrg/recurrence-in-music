0749a02a7bfc9c3e24bf5e97fac12289201b1344
Thiago Gomes <thiagoluiz92@gmail.com>
2019-09-13 13:55:11 -0300

    to mexendo no f_ref para reduzir as vezes que os loops sao chamados

--- v3/f_ref.py
+++ v3/f_ref.py
@@ -35,27 +35,46 @@ def uc(linha,ppq):
     uc = ((ppq*4)/linha[1][1])*linha[1][0]
     return uc
 
-def locR(linha,mapa,ppq):
-    ref = locdur(linha,mapa[0])
+#essas funcoes chamam locdur e compbpm mais vezes que o necessário
+def locR(linha,mapa,ppq,ref=list()):
+    if ref == []:
+        ref = locdur(linha,mapa[0])
     locR = (linha[1] - ref[0])/uc(ref,ppq)
     return locR
 
-def locT(linha,mapa,ppq):
-    ref = locdur(linha, mapa[0])
-    locT = ((locR(linha,mapa,ppq)%1)*ref[1][0])+1
+def locT(linha,mapa,ppq,ref=list()):
+    if ref == []:
+        ref = locdur(linha,mapa[0])
+    locT = ((locR(linha,mapa,ppq,ref)%1)*ref[1][0])+1
     return locT
 
-def locC(linha,mapa,ppq):
-    ref = locdur(linha,mapa[0])
-    locC = int(locR(linha,mapa,ppq) + ref[2][0])
+def locC(linha,mapa,ppq,ref=list()):
+    if ref == []:
+        ref = locdur(linha,mapa[0])
+    locC = int(locR(linha,mapa,ppq,ref) + ref[2][0])
     return locC
 
-def durR(linha,mapa,ppq):
-    ref = locdur(linha,mapa[0])
-    durR = locR(linha,mapa,ppq)*ref[1][0]
+def durR(linha,mapa,ppq,ref=list()):
+    if ref == []:
+        ref = locdur(linha,mapa[0])
+    durR = locR(linha,mapa,ppq,ref)*ref[1][0]
     return durR
 
-def durI(linha,mapa,ppq):
-    ref = locdur(linha,mapa[0])
-    durI = durR(linha,mapa,ppq) + ref[3]
+def durI(linha,mapa,ppq,ref=list()):
+    if ref == []:
+        ref = locdur(linha,mapa[0])
+    durI = durR(linha,mapa,ppq,ref) + ref[3]
     return durI
+
+def tudo(linha,mapa,ppq)
+    refcomp = compbpm(linha[0],mapa)
+    refbpm = compbpm(linha[1],mapa)
+    reflocdur = locdur(linha,mapa)
+    comp = refcomp[1]
+    bpm = refbpm[1]
+    locR
+    locC
+    locT
+    durR
+    durI
+    return
