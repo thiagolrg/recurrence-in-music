5122a9402433eccfc4226cb3d80362eca5b56134
Thiago Gomes <thiagoluiz92@gmail.com>
2019-10-09 12:38:13 -0300

    tinha apagado a linha sem querer

--- v3/limpaextraimapa.py
+++ v3/limpaextraimapa.py
@@ -11,10 +11,11 @@ for caminho in listacaminhos:
     entrada = f_l.entrada_midi(caminho)
     entradalimpa = f_l.limpeza(entrada)
     nomemusica = f_l.tira_nome(caminho)
-    
+    tom, modo, ppq, compassos, tempos, notas = f_l.limpa_extrai(entradalimpa)
+
     mapabpm = f2_m.mapa_bpm(tempos,compassos)
     mapacomploc = f2_m.mapa_locdur(compassos,ppq)
-    #as formulas do mappcomploc e do mapaseg são muito similares,
+    #as formulas do mappcomploc (f1_m) e do mapaseg(f1_seg) são muito similares,
     #talvez tenha um jeito de fazer formulas unicas ou reduzir a quantidade de formulas
     mapaseg = f2_s.mapa_seg(notas, mapacomploc, mapabpm, ppq)
 
