a41ab0b3af18eacdd285b2b77f9a6c8d6fb08a47
Thiago Gomes <thiagoluiz92@gmail.com>
2019-10-15 15:39:09 -0300

    fazendo funcao para tirar o nested, preciso mudar a forma de comparar por str()

--- v3/f_analise.py
+++ v3/f_analise.py
@@ -1,5 +1,56 @@
 import f_diretorios as f_d
 
+def interdurunicos_loc(interdurunicoloc, mapamus, nomemapamus):
+    for v, voz in mapamus['vozes'].items():
+        for posicao1 in range(len(voz['inte'])):
+            for posicao2 in range(posicao1, len(voz['inte'])):
+                locA = (posicao1, posicao2+1)
+                inteseg = tuple(voz['inte'][posicao1:posicao2+1])
+                durseg = tuple(voz['dur'][posicao1:posicao2+1])
+                intedurseg = (inteseg, durseg)
+                locC = voz['locC'][posicao1]
+                locT = voz['locT'][posicao1]
+                tamanho = ((posicao2 + 1) - posicao1)
+                valor = (mapamus['nome'], v, locC, locT, tamanho, locA)
+
+                interdurunicoloc.setdefault(intedurseg, []).append(valor)
+                if type(interdurunicoloc[intedurseg][0]) != dict:
+                    interdurunicoloc[intedurseg].insert(0 ,{'name' : set()})
+                interdurunicoloc[intedurseg][0]['name'].add(mapamus['nome'])
+    print(nomemapamus+' analisado')
+    return (interdurunicoloc)
+
+def filtro_maisde1musica(dicio):
+    pronto = {}
+    for chave, valor in sorted(dicio.items(), key=sort_tamanhoSI, reverse=True):
+        if len(valor[0]['name']) > 1:
+            pronto.setdefault(chave,valor[1:])
+
+    for chave1, valores1 in (pronto.copy()).items():
+        for chave2, valores2 in (pronto.copy()).items():
+            if len(chave2[0]) < len(chave1[0]) and str(chave2[0]).strip('()') in str(chave1[0]) and str(chave2[1]).strip('()') in str(chave1[1]):
+                a=0
+                for valor1 in valores1:
+                    p=0
+                    while p < len(valores2):
+                        if valores2[p][5][0] >= valor1[5][0] and valores2[p][5][1] <= valor1[5][1] and valores2[p][0:2] == valor1[0:2]:
+                            pronto[chave2].pop(pronto[chave2].index(valores2[p]))
+                            a = 1
+                        p += 1
+                if a == 1  and valores2 == []:
+                    pronto.pop(chave2)
+                elif a == 1 and len(set([x[0] for x in valores2])) == 1:
+                    pronto.pop(chave2)
+    print('filtro_maisde1musica ok')
+    return pronto  
+
+def sort_tamanhoSI(item):
+    return len(item[0][0])
+
+def sort_quantidadeLOC(item):
+    return len(item[1])  
+
+'''                
 def interunicos_loc(mapamus):
     interunicosloc = {}
     v = 0
@@ -18,41 +69,7 @@ def interunicos_loc(mapamus):
             interunicosloc2.setdefault(chave,valor)
     return interunicosloc
 
-def sort_tamanhoSI(item):
-    return len(item[0][0])
-
-def sort_quantidadeLOC(item):
-    return len(item[1])
-
-def interdurunicos_loc(interdurunicoloc, mapamus, nomemapamus):
-    for v, voz in mapamus['vozes'].items():
-        for posicao1 in range(len(voz['inte'])):
-            for posicao2 in range(posicao1, len(voz['inte'])):
-
-                inteseg = tuple(voz['inte'][posicao1:posicao2+1])
-                durseg = tuple(voz['dur'][posicao1:posicao2+1])
-                intedurseg = (inteseg, durseg)
-                locC = voz['locC'][posicao1]
-                locT = voz['locT'][posicao1]
-                tamanho = ((posicao2 + 1) - posicao1)
-                valor = (mapamus['nome'], v, locC, locT, tamanho)
-
-                interdurunicoloc.setdefault(intedurseg, []).append(valor)
-                if type(interdurunicoloc[intedurseg][0]) != dict:
-                    interdurunicoloc[intedurseg].insert(0 ,{'name' : set()})
-                interdurunicoloc[intedurseg][0]['name'].add(mapamus['nome'])
-    print(nomemapamus+' analisado')
-    return (interdurunicoloc)
-
-def filtro_maisde1musica(dicio):
-    pronto = {}
-    for chave, valor in sorted(dicio.items(), key=sort_tamanhoSI, reverse=True):
-        if len(valor[0]['name']) > 1:
-            pronto.setdefault(chave,valor[1:])
-    print('filtro_maisde1musica ok')
-    return pronto    
-
-'''                musica.setdefault((v, locCvoz[posicao1], locTvoz[posicao1], (posicao2+1)-posicao1), 
+musica.setdefault((v, locCvoz[posicao1], locTvoz[posicao1], (posicao2+1)-posicao1), 
                                  (tuple(intevoz[posicao1:posicao2+1]),
                                  tuple(durvoz[posicao1:posicao2+1]),
                                  tuple(compvoz[posicao1:posicao2+1]),
--- v3/mapamus_analise.py
+++ v3/mapamus_analise.py
@@ -4,6 +4,7 @@ import os
 
 dimapamus = f_d.diretorio('ler','.mapamus')
 dianalise = f_d.diretorio('gravar', '.analise')+'\\'+'analise'
+nomeanalise = input('nome do arquivo de analise: ')
 caminhosmapamusler = f_d.caminhos_arquivo(dimapamus, '.mapamus')
 os.makedirs(dianalise, exist_ok=True)
 
@@ -16,5 +17,11 @@ for caminho in caminhosmapamusler:
     print(caminhosmapamusler.index(caminho)+1,' de ',len(caminhosmapamusler))
 
 interdurunicoloc = f_a.filtro_maisde1musica(interdurunicoloc)
-f_d.escreve_arquivo(dianalise, 'interdurunico_loc.analise', interdurunicoloc, 'wb')
+f_d.escreve_arquivo(dianalise, nomeanalise+'.analise', interdurunicoloc, 'wb')
+
+with open(dianalise+'\\'+nomeanalise+'.txt', 'w') as f:
+    linha = 0
+    for items in interdurunicoloc.items():
+        linha = linha + 1
+        print(linha,'.   ',items,'\n',file=f)
 
