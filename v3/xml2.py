3480baf7d6a50d432df4938bf389287bc8bc257b
Thiago Gomes <thiagoluiz92@gmail.com>
2019-10-28 23:40:37 -0300

    xml to mapamus funcionando

--- v3/xml2.py
+++ v3/xml2.py
@@ -1,74 +1,139 @@
-with open(r'C:\Users\Thiago.DESKTOP-13409IC\Desktop\Análise\MusicXML\Teste4.xml') as f:
-    xmllista = []
+with open(r'C:\Users\Thiago.DESKTOP-13409IC\Desktop\Análise\MusicXML\MidiStressTest.xml') as f:
+    xml = []
     for linha in f.readlines():
         linha = linha.strip()
+        if '<!' in linha:
+            continue
+        linha = linha.replace('"', '')
         linha = linha.replace('\n', '')
-        xmllista.append(linha)
+        xml.append(linha)
 
 dicio = {}
 counter = 0
 duration = 0
-for l in xmllista:
-    if '<part id=' in l:
-        l = l.lstrip('<part id="') 
-        l = l.rstrip('">')
+tie = False
+p = 0
+while p < len(xml):
+    if '<part id=' in xml[p]:
+        l = xml[p].lstrip('<part id=')
+        l = l.rstrip('>')
         partID = str(l)
         dicio.setdefault(partID,{})
-
-    elif '</part>' in l:
+        p += 1
+        
+    elif '</part>' in xml[p]:
         counter = 0
-
-    elif '<divisions>' in l:
-        l = l.lstrip('<divisions>') 
+        p += 1
+        
+    elif '<divisions>' in xml[p]:
+        l = xml[p].lstrip('<divisions>') 
         l = l.rstrip('</divisions>')
         divisions = int(l)
-
-    elif '<duration>' in l:
-        l = l.lstrip('<duration>') 
-        l = l.rstrip('</duration>')
-        duration = int(l)
-        counter = counter + duration
-
-    elif '<measure number= "' in l:
-        l = l.lstrip('<measure number= "') 
-        l = l.rstrip('" width="694">')
+        p += 1
+        
+    elif '<measure number=' in xml[p]:
+        l = xml[p].lstrip('<measure number=') 
+        l = l.split(' ')[0]
         measureNumber = int(l)
+        p += 1
 
-    elif '<beats>' in l:
-        l = l.lstrip('<beats>') 
-        l = l.rstrip('</beats>')
-        beats = int(l)
+    elif '<time ' in xml[p]:
+        timeList = []
+        for p2 in range(p,len(xml)):
+            if '</time>' in xml[p2]:
+                timeList.append(xml[p2])
+                p += 1
+                break
+            timeList.append(xml[p2])
+            p += 1
+        for linha in timeList:
+            if '<beats>' in linha:
+                l = linha.lstrip('<beats>') 
+                l = l.rstrip('</beats>')
+                beats = int(l)
+            elif '<beat-type>' in linha:
+                l = linha.lstrip('<beat-type>') 
+                l = l.rstrip('</beat-type>')
+                beatType = int(l)
+        time = tuple([counter,(beats, beatType)])
 
-    elif '<beat-unit>' in l:
-        l = l.lstrip('<beat-unit>') 
-        l = l.rstrip('</beat-unit>')
-        beatsUnit = str(l)
+    elif '<metronome ' in xml[p]:
+        metronomeList = []
+        for p2 in range(p,len(xml)):
+            if '</metronome>' in xml[p2]:
+                metronomeList.append(xml[p2])
+                p += 1
+                break
+            metronomeList.append(xml[p2])
+            p += 1
+        for linha in metronomeList:
+            if '<beat-unit>' in linha:
+                l = linha.lstrip('<beat-unit>') 
+                l = l.rstrip('</beat-unit>')
+                beatUnit = str(l)
+            elif '<per-minute>' in linha:
+                l = linha.lstrip('<per-minute>') 
+                l = l.rstrip('</per-minute>')
+                perMinute = int(l)
+        metronome = tuple([beatUnit, perMinute])
 
-    elif '<per-minute>' in l:
-        l = l.lstrip('<per-minute>') 
-        l = l.rstrip('</per-minute>')
-        perMinute = int(l)
+    elif '<note ' in xml[p]:
+        noteList = []
+        alter = None
+        for p2 in range(p,len(xml)):
+            if '</note>' in xml[p2]:
+                noteList.append(xml[p2])
+                p += 1
+                break
+            noteList.append(xml[p2])
+            p += 1
+        for linha in noteList:
+            if '<rest />' in linha:
+                step = None
+                octave = None
+            elif '<step>' in linha:
+                l = linha.lstrip('<step>') 
+                l = l.rstrip('</step>')
+                step = str(l)
+            elif '<alter>' in linha:
+                l = linha.lstrip('<alter>') 
+                l = l.rstrip('</alter>')
+                alter = int(l)
+            elif '<octave>' in linha:
+                l = linha.lstrip('<octave>') 
+                l = l.rstrip('</octave>')
+                octave = int(l)
+            elif '<duration>' in linha:
+                l = linha.lstrip('<duration>') 
+                l = l.rstrip('</duration>')
+                if tie == True:
+                    duration = int(l) + duration
+                else:
+                    duration = int(l)
+            elif '<tie type=start />' in linha:
+                tie = True
+            elif '<tie type=stop />' in linha:
+                tie = False
 
-    elif '<beat-type>' in l:
-        l = l.lstrip('<beat-type>') 
-        l = l.rstrip('</beat-type>')
-        beatType = int(l)
+        if tie == False:
 
-    elif '<step>' in l:
-        l = l.lstrip('<step>') 
-        l = l.rstrip('</step>')
-        step = str(l)
-        dicio[partID].setdefault('step',[]).append(tuple([counter,step]))
+            ut = (divisions*4)/time[1][1]
+            uc = ut*time[1][0]
+            durationTime = counter - time[0]
+            while durationTime//uc > 0:
+                durationTime = durationTime - uc
+            durationTime = durationTime/ut
 
-    elif '<alter>' in l:
-        l = l.lstrip('<alter>') 
-        l = l.rstrip('</alter>')
-        alter = int(l)
-
-    elif '<octave>' in l:
-        l = l.lstrip('<octave>') 
-        l = l.rstrip('</octave>')
-        octave = int(l)
-
+            dicio[partID].setdefault('step', []).append(step)
+            dicio[partID].setdefault('alter', []).append(alter)
+            dicio[partID].setdefault('octave', []).append(octave)
+            dicio[partID].setdefault('metronome', []).append(metronome)
+            dicio[partID].setdefault('time', []).append(time)
+            dicio[partID].setdefault('counter', []).append(counter)
+            dicio[partID].setdefault('measureNumber', []).append(measureNumber)
+            dicio[partID].setdefault('durationTime',[]).append(durationTime)
+            counter = counter + duration
+    else:
+        p += 1
 
 debug = 0
