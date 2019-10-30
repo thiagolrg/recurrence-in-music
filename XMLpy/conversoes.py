6d157346be12628f330b067b6c8a337b7e173ae4
Thiago Gomes <thiagoluiz92@gmail.com>
2019-10-30 19:33:20 -0300

    fazendo função para intervalos diatonicos

--- XMLpy/conversoes.py
+++ XMLpy/conversoes.py
@@ -24,3 +24,22 @@ def duration_time(divisions,time,counter):
     while durationTime//uc > 0:
         durationTime = durationTime - uc
     return durationTime/ut
+
+def int_diatonic(nota1,nota2):
+    step_string = 'CDEFGABCDEFGABCDEFGABC'
+    oit = -1
+    int_lista = [-7,-6,-5,-4,-3,-2,1,2,3,4,5,6,7]
+
+    for p in range(len(step_string)):
+        if step_string[p] == nota1[0]:
+            step_string =  step_string[p+1:p+14]
+            break
+    
+    oit_list = []
+    for nota in step_string:
+        if nota == 'C':
+            oit += 1
+        oit_list.append(oit)
+    return step_string
+
+int_diatonic(('C',3),('B',3))
