6430e7a0232585ebfe3e47cdf7dd3d245929948a
Thiago Gomes <thiagoluiz92@gmail.com>
2019-11-07 23:37:03 -0300

    nao to conseguindo fazer os input analise filtro

--- XMLpy/inpfiltro.py
+++ XMLpy/inpfiltro.py
@@ -1,27 +1,38 @@
-def inpchaveV(listachaves):
-    chaveV = input('chaveV :')
-    if chaveV in musD.keys()
-        return chaveV
+listaC = ['a','b','c','d']
+listaT = ['a','b','c','d']
+listaFT = ['a','b','c','d']
+
+listachave = []
+listavalor = []
+for valor in listaC:
+    cv = input(valor,'chave ou valor?', ['chave','valor','proximo'])
+    t = input(valor,['como extrair?',listaT])
+
+    
+
+def inp(texto, opcoes):
+    print(f'{texto}\n')
+    for o in opcoes:
+        print(o)
+    ip = input('escolha uma opcao: ')
+    if ip in opcoes:
+        return ip
     else:
-        print('valor não existe no dicionário')
-        return inpchaveV()
+        print(f'{ip} não é uma ooção')
+        return inp(texto, opcoes)
 
-def inpchaveT(listaTA):
-    tipo = input('chaveT :')
-    if tipo in listaTA
-        return tipo
-    else:
-        print('tipo não existe no dicionário')
-        return inpchaveT()
-
-def inpfiltroQT():
-def inpfiltroVL():
-def inpfiltroT():
-
-def inpchave():
-    chaveV = inpchaveV()
-    chaveT = inpT()
+def chavevalor_VT(listaC, listaT):
+    chaveV = inp('qual chave extrair:', listaC)
+    chaveT = inp('como extrair chave:', listaT)
     return (chaveV, chaveT)
+
+def denovo(lista, listaC, listaT):
+    lista.append(chavevalor_VT(listaC,listaT))
+    outra = inp('outra chave?', ['s','n'])
+    if outra == 's':
+        return denovo(lista,listaC,listaT)
+    else:
+        return lista
 
 
 
--- XMLpy/xml2.py
+++ XMLpy/xml2.py
@@ -19,32 +19,15 @@ for xml in caminhosxml:
     musD.setdefault('nome',nome)
     f_d.escreve_pickle(diD, musD, nome)
 
-listaC = [x for x in musD.keys()]
-listaTA = ['p1', 'p1p2', 'p1p2m1','p1set', 'p1p2set', 'p2m1set']
-listaTF = ['p1f', 'p1p2f', 'p2m1f']
 
-def inp(opcoes, prompt):
-    for o in opcoes:
-        print(o)
-    ip = input(f'{prompt}: ')
-    if ip in opcoes:
-        return ip
-    else:
-        return inp(opcoes, prompt)
 
-def inpchave(lista, listaC,listaTA):
-    print('opcoes para recorrencia: ')
-    chaveV = inp(listaC, 'escolha uma opcao')
-    tipoT = inp(listaTA, 'escolha um tipo')
-    lista.append((chaveV,tipoT))
-    print('outra chave?')
-    outra = inp(('s','n'), 'escolha s ou n')
-    if outra == 's':
-        lista.apppend(inpchave(lista, listaC, listaTA))
-    if outra == 'n':
-        return lista
-
-teste = inpchave([], listaC, listaTA)
+caminhosdict = f_d.caminhos_extensao(diD, '.p')
+for caminho in caminhosdict:
+    musD = f_d.le_pickle(caminho)
+    listaC = [x for x in musD['P1'].keys()]
+    listaTA = ['p1', 'p1p2', 'p1p2m1','p1set', 'p1p2set', 'p2m1set']
+    listaTF = ['p1f', 'p1p2f', 'p2m1f']
+    teste = apachave([], listaC, listaTA)
 
 '''
 aDict = {}
