3af4fd0117b3d4409a01ca9b1d0ca906bdd59ef7
ThiagoLRG <54580798+ThiagoLRG@users.noreply.github.com>
2021-04-27 12:50:45 -0300

    salvando novo filtro por quantidade

--- XMLpy/principal.py
+++ XMLpy/principal.py
@@ -45,12 +45,16 @@ ListaSegmentosCaracteristicas = [[('intDia', 'p1p2')],
                                 [('intDia', 'p1p2'), ('duracao', 'p1p2'), ('Ntempo', 'p1'), ('Ptempo', 'p1')]]
 
 
-def semcontinte_porquantidade(segmentacao, caminhosdict, diA, SegmentosCaracteristicas, igualouigualemaior, distancia):
+def porquantidade_conj(segmentacao, caminhosdict, diA, SegmentosCaracteristicas, diatancia):
+    print('por quantidade conjuntos:\n')
+
     quantidadeT = len(caminhosdict)
     nomes = [f_d.caminho_nome(x, ['.p']) for x in caminhosdict]
+    igualouigualemaior = '>='
     for quantidade in range(1,quantidadeT+1):
-        emquantidade = f_sf.porquantidade(segmentacao, quantidade, igualouigualemaior)
+        emquantidade = f_sf.porquantidade2(segmentacao, quantidade, igualouigualemaior)
         if len(emquantidade) == 0:
+            q = quantidade
             break
         print(f'{igualouigualemaior} a {quantidade} de {quantidadeT} ')
         
@@ -59,36 +63,63 @@ def semcontinte_porquantidade(segmentacao, caminhosdict, diA, SegmentosCaracteri
         parametros.setdefault('quantidade', [quantidadeT])
         parametros.setdefault('analise', [f'{SegmentosCaracteristicas} em {igualouigualemaior} {quantidade} SemContInte Distancia: {distancia}'])
         f_d.escreve_txt(diA, parametros, nomeanalise)
-        f_d.escreve_txt(diA, {x:y for x,y in emquantidade}, nomeanalise)
-
-#Em cada música em várias combinações de característisticas
-print('por musica:\n')
-for i in range(len(caminhosdict)):
-    print(f'{i+1} de {len(caminhosdict)}\n')
-    caminho = [caminhosdict[i]]
-
-    for SegmentosCaracteristicas in ListaSegmentosCaracteristicas:
-        segmentacao = f_sf.Segmentacao(SegmentosCaracteristicas, LocalizacoesCaracteristicas, caminho, diA)
-"""
-        distancia = 1
-        segmentacao = f_sf.sem_cont_inte(segmentacao, distancia=distancia)
-
-        quantidadeT = len(caminhosdict)
-        nomes = [f_d.caminho_nome(x, ['.p']) for x in caminhosdict]
+        f_d.escreve_txt_grupos(diA, emquantidade, nomeanalise)
+    else:
+        q = quantidadeT+1
+    
+    igualouigualemaior = '=='
+    for quantidade in range(1,q):
+        emquantidade = f_sf.porquantidade2(segmentacao, quantidade, igualouigualemaior)
+        if len(emquantidade) == 0:
+            continue
+        print(f'{igualouigualemaior} a {quantidade} de {quantidadeT} ')
         nomeanalise = 'analise'+str(len(f_d.caminhos_extensoes(diA, ['.txt']))+1)
         parametros = {'nomes': nomes}
         parametros.setdefault('quantidade', [quantidadeT])
-        parametros.setdefault('analise', [f'{SegmentosCaracteristicas} SemContInte Distancia: {distancia}'])
+        parametros.setdefault('analise', [f'{SegmentosCaracteristicas} em {igualouigualemaior} {quantidade} SemContInte Distancia: {distancia}'])
         f_d.escreve_txt(diA, parametros, nomeanalise)
-        f_d.escreve_txt(diA, {x:y for x,y in segmentacao}, nomeanalise)
-"""
+        f_d.escreve_txt_grupos(diA, emquantidade, nomeanalise)
+    print()
 
-print('por quantidade:\n')
+def porquantidade_geral(segmentacao, caminhosdict, diA, SegmentosCaracteristicas, distancia):
+    print('por quantidade geral:\n')
+    quantidadeT = len(caminhosdict)
+    nomes = [f_d.caminho_nome(x, ['.p']) for x in caminhosdict]
+    igualouigualemaior = '>='
+    for quantidade in range(1,quantidadeT+1):
+        emquantidade = f_sf.porquantidade(segmentacao, quantidade, igualouigualemaior)
+        if len(emquantidade) == 0:
+            q = quantidade
+            break
+        print(f'{igualouigualemaior} a {quantidade} de {quantidadeT} ')
+        nomeanalise = 'analise'+str(len(f_d.caminhos_extensoes(diA, ['.txt']))+1)
+
+        parametros = {'nomes': nomes}
+        parametros.setdefault('quantidade', [quantidadeT])
+        parametros.setdefault('analise', [f'{SegmentosCaracteristicas} em {igualouigualemaior} {quantidade} SemContInte Distancia: {distancia}'])
+        f_d.escreve_txt(diA, parametros, nomeanalise)
+        f_d.escreve_txt(diA, {c:v for c,v in emquantidade}, nomeanalise)
+    else:
+        q = quantidadeT+1
+    
+    igualouigualemaior = '=='
+    for quantidade in range(1,q):
+        emquantidade = f_sf.porquantidade(segmentacao, quantidade, igualouigualemaior)
+        if len(emquantidade) == 0:
+            continue
+        print(f'{igualouigualemaior} a {quantidade} de {quantidadeT} ')
+
+        nomeanalise = 'analise'+str(len(f_d.caminhos_extensoes(diA, ['.txt']))+1)
+        parametros = {'nomes': nomes}
+        parametros.setdefault('quantidade', [quantidadeT])
+        parametros.setdefault('analise', [f'{SegmentosCaracteristicas} em {igualouigualemaior} {quantidade} SemContInte Distancia: {distancia}'])
+        f_d.escreve_txt(diA, parametros, nomeanalise)
+        f_d.escreve_txt(diA, {c:v for c,v in emquantidade}, nomeanalise)
+    print()
+
 for SegmentosCaracteristicas in ListaSegmentosCaracteristicas:
     segmentacao = f_sf.Segmentacao(SegmentosCaracteristicas, LocalizacoesCaracteristicas, caminhosdict, diA)
-"""
     distancia = 1
-    segmentacao = f_sf.sem_cont_inte(segmentacao, distancia=distancia)
-    semcontinte_porquantidade(segmentacao, caminhosdict, diA, SegmentosCaracteristicas, '==', distancia)
-    semcontinte_porquantidade(segmentacao, caminhosdict, diA, SegmentosCaracteristicas, '>=', distancia)
-"""
+    segmentacao = f_sf.sem_cont_inte_conj(segmentacao, distancia=distancia, conj=1)
+    porquantidade_conj(f_sf.dictConj(segmentacao, 1), caminhosdict, diA, SegmentosCaracteristicas, distancia)
+    porquantidade_geral(segmentacao, caminhosdict, diA, SegmentosCaracteristicas, distancia)
