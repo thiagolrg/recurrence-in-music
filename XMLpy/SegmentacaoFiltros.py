eee77ba4039120cd2dcc0838e94e08cced09b3f5
ThiagoLRG <54580798+ThiagoLRG@users.noreply.github.com>
2021-04-27 12:45:55 -0300

    novo filtro por conjuntos e apaguei antigos

--- XMLpy/SegmentacaoFiltros.py
+++ XMLpy/SegmentacaoFiltros.py
@@ -152,6 +152,7 @@ def sem_cont(listarecorrencias):
     print(f'{stop-start} segundos\n')
     return sort_recorrencias(semcont)
 
+#Essas funções são compartilhadas pelos dois filtros, ordenação e condicao para contidos e inercalados
 def sort_recorrencias(segmentacao):
     return sorted([(c, v) for c, v in segmentacao], key=lambda item: (len(item[0][0]), len(item[1])), reverse=True)
 def contida(listaposicoes, posicao):
@@ -167,153 +168,72 @@ def intercalada(listaposicoes, posicao, distancia=0):
             return True
     return False
 
-#Filtro por conjuntos 1
-#setgrupo=1 retira todas na mesma combinacao de músicas, setgrupo=3 retira todas na mesma combinacao de vozes
-def sem_cont_inte_conj1(listarecorrencias, distancia=0, setgrupo=1):
-    listarecorrencias = sort_cont_conj1(listarecorrencias, setgrupo)
-    print(f'sem cont inte:')
+
+#Novo Filtro por conjuntos
+#conj = 1 monta conjuntos de músicas, conj = 3 monta conjuntos de vozes, gostei mais do 1
+def sem_cont_inte_conj(listarecorrencias, distancia, conj):
+    print(f'sem cont inte conj:')
     start = time.perf_counter()
+    listarecorrencias = sort_recorrencias(listarecorrencias)
+    listarecorrencias = dictConj(listarecorrencias, conj)
     semcontinte = []
-    for grupo, segspos in listarecorrencias:
+    denovo = False
+    for grupo, segpos in listarecorrencias.items():
         quepassaram = []
-        for segmento, posicoes in segspos:
+        for segmentoposicoes in segpos:
             posicoessegmento = []
-            for posicao in posicoes:
+            for posicao in segmentoposicoes[1]:
                 if not contida(quepassaram, posicao) and not intercalada(posicoessegmento, posicao, distancia=distancia) and not intercalada(quepassaram, posicao, distancia=distancia):
                     posicoessegmento.append(posicao)
-            setv = set()
-            for v in posicoessegmento:
-                quepassaram.append(v)
-                setv.add(v[0:setgrupo])
-            if tuple(sorted(setv)) == grupo:
-                semcontinte.append((segmento,posicoessegmento))
-            print(f'\rQuaSegUnicos: {len(semcontinte)} ', end='')
+            if len(posicoessegmento) > 1:
+                for v in posicoessegmento:
+                    quepassaram.append(v)
+                gruponovo = tuple(sorted({p[0:conj] for p in posicoessegmento}))
+                if grupo != gruponovo:
+                    denovo = True
+                semcontinte.append((segmentoposicoes[0],posicoessegmento))
     stop = time.perf_counter()
     dadosseg(semcontinte)
     print(f'{stop-start} segundos\n')
-    return sorted([(c, v) for c, v in semcontinte if len(v) > 1], key=lambda item: (len(item[0][0]), len(item[1])), reverse=True)
+    if denovo:
+        return sem_cont_inte_conj(semcontinte, distancia, conj)
+    return sort_recorrencias(semcontinte)
 
-def sort_cont_conj1(listarecorrencias, setgrupo):
-    #recorrencias em organizadas posicao, conjunto e segmentos
-    p_pset_pseg = []
-    for seg, pos in listarecorrencias:
-        posset = {p[0:setgrupo] for p in pos}
-        for p in pos:
-            p_pset_pseg.append((p,tuple(sorted(posset)),seg))
-    #sort por nome, conjunto, tamanho maior, posicao menor
-    p_pset_pseg = sorted([(p, pset, pseg) for p, pset, pseg in p_pset_pseg], key=lambda item: (item[0][3][0]))
-    p_pset_pseg = sorted([(p, pset, pseg) for p, pset, pseg in p_pset_pseg], key=lambda item: (len(item[2][0])), reverse=True)
-    p_pset_pseg = sorted([(p, pset, pseg) for p, pset, pseg in p_pset_pseg], key=lambda item: (item[1]))
-    p_pset_pseg = sorted([(p, pset, pseg) for p, pset, pseg in p_pset_pseg], key=lambda item: (item[0][0:3]))
-
-    locpset = defaultdict(list)
-    for p, pset, pseg in p_pset_pseg:
-        locpset[(p[0:3], pset)].append((p,pseg))
-
-    listapronta = []
-    for grupo, ppsegs in locpset.items():
-        diciogrupos = defaultdict(list)
-        for p, pseg in ppsegs:
-            diciogrupos[pseg].append(p)
-        diciogrupos = [(c,v) for c, v in diciogrupos.items()]
-        listapronta.append([grupo[1], diciogrupos])
-    return listapronta
-
-#Filtro por conjuntos 2
-#setgrupo=1 retira todas na mesma combinacao de músicas, setgrupo=3 retira todas na mesma combinacao de vozes
-def sem_cont_conj2(listarecorrencias, setgrupo=1):
-    print(f'filtro sem cont:')
+def sem_cont_conj(listarecorrencias, distancia, conj):
+    print(f'sem cont conj:')
     start = time.perf_counter()
-    listarecorrencias = sort_cont_conj2(listarecorrencias,setgrupo)
-    dictrecorrencias = defaultdict(list)
-    for grupo in listarecorrencias:
+    listarecorrencias = sort_recorrencias(listarecorrencias)
+    listarecorrencias = dictConj(listarecorrencias, conj)
+    semcontinte = []
+    denovo = False
+    for grupo, segpos in listarecorrencias.items():
         quepassaram = []
-        for posicao in grupo[1]:
-            if not contida2(quepassaram, posicao[0]):
-                quepassaram.append(posicao)
-        for posicao, segmento in quepassaram:
-            dictrecorrencias[(segmento, grupo[0])].append(posicao)
+        for segmentoposicoes in segpos:
+            posicoessegmento = []
+            for posicao in segmentoposicoes[1]:
+                if not contida(quepassaram, posicao):
+                    posicoessegmento.append(posicao)
+            if len(posicoessegmento) > 1:
+                for v in posicoessegmento:
+                    quepassaram.append(v)
+                gruponovo = tuple(sorted({p[0:conj] for p in posicoessegmento}))
+                if grupo != gruponovo:
+                    denovo = True
+                semcontinte.append((segmentoposicoes[0],posicoessegmento))
     stop = time.perf_counter()
-    print('quantidade tudo:')
-    dadosseg(dictrecorrencias)
-    t = stop-start
-    start = time.perf_counter()
-    listarecorrenciaspronta = []
-    for chave, valor in dictrecorrencias.items():
-        if len(valor) > 1:
-            setv = {v[0:setgrupo] for v in valor}
-            if tuple(sorted(setv)) == chave[1]:
-                listarecorrenciaspronta.append((chave[0], valor))
-    stop = time.perf_counter()
-    t = t+stop-start
-    print('quantidade so recorrencias:')
-    dadosseg(listarecorrenciaspronta)
-    print(f'{t} segundos\n')
-    return sorted([(c, v) for c, v in listarecorrenciaspronta if len(v) > 1], key=lambda item: (len(item[0][0]), len(item[1])), reverse=True)
+    dadosseg(semcontinte)
+    print(f'{stop-start} segundos\n')
+    if denovo:
+        return sem_cont_inte_conj(semcontinte, distancia, conj)
+    return sort_recorrencias(semcontinte)
 
-#setgrupo=1 retira todas na mesma combinacao de músicas, setgrupo=3 retira todas na mesma combinacao de vozes
-def sem_cont_inte_conj2(listarecorrencias, distancia=0, setgrupo=1):
-    print(f'filtro sem cont inte:')
-    start = time.perf_counter()
-    listarecorrencias = sort_cont_conj2(listarecorrencias, setgrupo)
-    dictrecorrencias = defaultdict(list)
-    for grupovoz in listarecorrencias:
-        quepassaram = []
-        for posicao in grupovoz[1]:
-            if not contida2(quepassaram, posicao[0]) and not intercalada2(quepassaram, posicao[0], distancia=distancia):
-                quepassaram.append(posicao)
-        for posicao, segmento in quepassaram:
-            dictrecorrencias[(segmento, grupovoz[0])].append(posicao)
-    stop = time.perf_counter()
-    print('quantidade tudo:')
-    dadosseg(dictrecorrencias)
-    t = stop-start
-    start = time.perf_counter()
-    listarecorrenciaspronta = []
-    for chave, valor in dictrecorrencias.items():
-        if len(valor) > 1:
-            setv = {v[0:setgrupo] for v in valor}
-            if tuple(sorted(setv)) == chave[1]:
-                listarecorrenciaspronta.append((chave[0], valor))
-    stop = time.perf_counter()
-    t = t+stop-start
-    print('quantidade so recorrencias:')
-    dadosseg(listarecorrenciaspronta)
-    print(f'{t} segundos\n')
-    return sorted([(c, v) for c, v in listarecorrenciaspronta if len(v) > 1], key=lambda item: (len(item[0][0]), len(item[1])), reverse=True)
+def dictConj(listarecorrencias, conj):
+    dictConj = defaultdict(list)
+    for segpos in listarecorrencias:
+        dictConj[tuple(sorted({p[0:conj] for p in segpos[1]}))].append(segpos)
+    return dictConj
 
-def sort_cont_conj2(listarecorrencias, setgrupo):
-    #recorrencias em organizadas posicao, conjunto e segmentos
-    p_pset_pseg = []
-    for seg, pos in listarecorrencias:
-        posset = {p[0:setgrupo] for p in pos}
-        for p in pos:
-            p_pset_pseg.append((p,tuple(sorted(posset)),seg))
-    #sort por nome, conjunto, tamanho maior, posicao menor
-    p_pset_pseg = sorted([(p, pset, pseg) for p, pset, pseg in p_pset_pseg], key=lambda item: (item[0][3][0]))
-    p_pset_pseg = sorted([(p, pset, pseg) for p, pset, pseg in p_pset_pseg], key=lambda item: (len(item[2][0])), reverse=True)
-    p_pset_pseg = sorted([(p, pset, pseg) for p, pset, pseg in p_pset_pseg], key=lambda item: (item[1]))
-    p_pset_pseg = sorted([(p, pset, pseg) for p, pset, pseg in p_pset_pseg], key=lambda item: (item[0][0:3]))
-
-    locpset = defaultdict(list)
-    for p, pset, pseg in p_pset_pseg:
-        locpset[(p[0:3], pset)].append((p,pseg))
-    return [(c[1],v) for c, v in locpset.items()]  
-def contida2(listaposicoes, posicao):
-    for quepassou in listaposicoes:
-        if posicao[0:3] == quepassou[0][0:3] and posicao[3][0] >= quepassou[0][3][0] and posicao[3][1] <= quepassou[0][3][1]:
-            return True
-    return False
-def intercalada2(listaposicoes, posicao, distancia):
-    for quepassou in listaposicoes:
-        if posicao[0:3] == quepassou[0][0:3] and posicao[3][0] > quepassou[0][3][0] and posicao[3][0] < quepassou[0][3][1]+distancia and posicao[3][1] > quepassou[0][3][1]:
-            return True
-        if posicao[0:3] == quepassou[0][0:3] and posicao[3][1] > quepassou[0][3][0]-distancia and posicao[3][1] < quepassou[0][3][1] and posicao[3][0] < quepassou[0][3][0]:
-            assert posicao[3][1] - posicao[3][0] < quepassou[0][3][1] - quepassou[0][3][0]
-            return True
-    return
-
-#Por Quantidade
+#Por Quantidade para antigo filtro geral
 def porquantidade(segmentacao, quantidade, iguaiouigualemaior):
     quepassaram = []
     for item in segmentacao:
@@ -322,4 +242,14 @@ def porquantidade(segmentacao, quantidade, iguaiouigualemaior):
             quepassaram.append(item)
         elif iguaiouigualemaior == '>=' and len(nomes) >= quantidade:
             quepassaram.append(item)
+    return quepassaram
+
+#Por quantidade para novo filtro conjuntos
+def porquantidade2(segmentacao, quantidade, iguaiouigualemaior):
+    quepassaram = defaultdict(list)
+    for key, value in segmentacao.items():
+        if iguaiouigualemaior == '==' and len(key) == quantidade:
+            quepassaram.update({key:value})
+        elif iguaiouigualemaior == '>=' and len(key) >= quantidade:
+            quepassaram.update({key:value})
     return quepassaram
